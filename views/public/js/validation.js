25d9cc2dabadd40f9859eb293c44f4e54ff9a0b2
Gabrielgit04 <gabrielmoises1202@gmail.com>
2025-06-30 06:34:10 +0700

    agregando ultima receta por default

--- views/public/js/validation.js
+++ views/public/js/validation.js
@@ -11,12 +11,25 @@ const instrucciones = formValidate.elements['instrucciones'].value;
 
 const ingredientes = formValidate.elements['ingredientes'].value;
 
-console.log(`Nombre de receta: ${nombre_receta}\nIngredientes:${ingredientes}\nIntrucciones:${instrucciones}`)
+const porcion = document.getElementById('porcion');
+
+porcion.addEventListener('input', (event) =>{
+    const textValue = event.currentTarget.value;
+
+    if (!isNaN(textInvalid(textValue))){
+        return formValidate.innerHtml = "Este campo debe contener valores numericos.";
+    }
+
+});
+
+function textInvalid(text){
+    return /["0-9"]/.test(text);
+}
+
 // funciones
 function validarFormulario() {
     if (nombre_receta.value.trim() === "" || ingredientes.value.trim() === "" || instrucciones.value.trim() === "") {
-        alert('El campo es obligatorio, por favor, rellenalo.');
-        return false;
+        return formValidate.innerHtml('El campo es obligatorio, por favor, rellenalo.');
     }
 
     if(!formValidate.checkValidity()){
